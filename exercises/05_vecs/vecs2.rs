31de264e24f41cce24633a53f9d5ce597f94464a
YùToutCourt <wapzif@gmail.com>
2024-04-23 13:49:37 +0200

    vecs done

--- exercises/05_vecs/vecs2.rs
+++ exercises/05_vecs/vecs2.rs
@@ -7,13 +7,21 @@
 //
 // Execute `rustlings hint vecs2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+/*
+
+Pour le vec_loop, on doit *element *= 2 pour chaque élément du vecteur.
+
+* pour déréférencer l'élément
+
+Pour vec_map, on doit utiliser la méthode map() pour appliquer une fonction à chaque élément du vecteur.
+
+*/
 
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
     for element in v.iter_mut() {
         // TODO: Fill this up so that each element in the Vec `v` is
         // multiplied by 2.
-        ???
+        *element *= 2;
     }
 
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
@@ -21,10 +29,10 @@ fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
 }
 
 fn vec_map(v: &Vec<i32>) -> Vec<i32> {
-    v.iter().map(|element| {
-        // TODO: Do the same thing as above - but instead of mutating the
+            // TODO: Do the same thing as above - but instead of mutating the
         // Vec, you can just return the new number!
-        ???
+    v.iter().map(|element| {
+        element * 2
     }).collect()
 }
 
