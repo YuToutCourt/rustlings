1ce208ce254a6d6461153059e701e8a36fc145d5
YùToutCourt <wapzif@gmail.com>
2024-04-23 12:40:47 +0200

    end 1.0

--- exercises/01_variables/variables6.rs
+++ exercises/01_variables/variables6.rs
@@ -3,9 +3,12 @@
 // Execute `rustlings hint variables6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+/*
+WRITE UP:
+In Rust, when you declare a constant using the const keyword, you need to specify the type of the constant. The Rust compiler needs to know the type of every variable at compile time.
+*/
 
-const NUMBER = 3;
+const  NUMBER: i32 = 3;
 fn main() {
     println!("Number {}", NUMBER);
 }
