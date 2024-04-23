04df36ac984c8723af2dfc94977cf4922ae4ab37
YùToutCourt <wapzif@gmail.com>
2024-04-23 15:02:38 +0200

    wirteup

--- exercises/06_move_semantics/move_semantics6.rs
+++ exercises/06_move_semantics/move_semantics6.rs
@@ -8,19 +8,24 @@
 /*
 WRITE-UP
 
+La fonction get_char prend ownership de la String data, donc on ne peut plus l'utiliser après. 
+Donc premier fix : get_char(&data); et get_char(data: &String); J'ai réjouter les & pour passer une référence à la fonction get_char
+
+Puis la fonction string_uppercase on doit remove les & pour prendre ownership de la String data.
+(Enfin c'est ce que je pense, je suis pas sûr de moi)
 
 */
 
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(data.clone());
+    get_char(&data);
 
     string_uppercase(data);
 }
 
 // Should not take ownership
-fn get_char(data: String) -> char {
+fn get_char(data: &String) -> char {
     data.chars().last().unwrap()
 }
 
