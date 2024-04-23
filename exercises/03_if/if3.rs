536da90915e63ff30e94f69dd7ecb303b824b117
YùToutCourt <wapzif@gmail.com>
2024-04-23 13:04:32 +0200

    if end

--- exercises/03_if/if3.rs
+++ exercises/03_if/if3.rs
@@ -2,17 +2,19 @@
 //
 // Execute `rustlings hint if3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+/*
+La valeur que identifier prend doit lier au if else de habitat. Donc soit 1, 2 ou 3. Pas de float ou de STR
+*/
 
 pub fn animal_habitat(animal: &str) -> &'static str {
     let identifier = if animal == "crab" {
         1
     } else if animal == "gopher" {
-        2.0
+        2
     } else if animal == "snake" {
         3
     } else {
-        "Unknown"
+        0
     };
 
     // DO NOT CHANGE THIS STATEMENT BELOW
