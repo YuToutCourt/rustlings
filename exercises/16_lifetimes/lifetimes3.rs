543ddcfae016d9a3c923c4f3e08893f75cd0e687
YùToutCourt <wapzif@gmail.com>
2024-04-24 08:27:19 +0200

    lifetime

--- exercises/16_lifetimes/lifetimes3.rs
+++ exercises/16_lifetimes/lifetimes3.rs
@@ -5,17 +5,32 @@
 // Execute `rustlings hint lifetimes3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+/*
+WRITE UP
+
+Pour corrige le problème de life time il y a 2 solutions.
+
+La première rajouter le life time à la structure book 
+struct Book<'a> {
+    author: &'a str,
+    title: &'a str,
+}
+
+La deuxième solution est de changer le type de author et title en String
+Vu que les String sont des types qui sont alloués sur le tas, il n'y a pas de problème de life time
+Cette solution est plus simple donc je vais la choisir
+
+*/
 
 struct Book {
-    author: &str,
-    title: &str,
+    author: String,
+    title: String,
 }
 
 fn main() {
     let name = String::from("Jill Smith");
     let title = String::from("Fish Flying");
-    let book = Book { author: &name, title: &title };
+    let book = Book { author: name, title: title };
 
     println!("{} by {}", book.title, book.author);
 }
