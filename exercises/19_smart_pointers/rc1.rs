27d7430458fd9781def8d3ee4b7e7d7ffbb5300e
YùToutCourt <wapzif@gmail.com>
2024-04-24 10:36:37 +0200

    smart pointer

--- exercises/19_smart_pointers/rc1.rs
+++ exercises/19_smart_pointers/rc1.rs
@@ -10,7 +10,17 @@
 //
 // Execute `rustlings hint rc1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+/*
+WRITE UP:
+
+Lorsque que les planètes Saturn, Uranus et Neptune sont crée, de base on leur donné une nouvelle instance de Sun.
+Alors qu'on veut la même instance de Sun pour toutes les planètes. Donc on doit cloner la référence de Sun pour chaque planète.
+
+Puis ensuite il manque des drop() pour libérer la mémoire de Sun de chaque planète.
+
+
+*/
+
 
 use std::rc::Rc;
 
@@ -61,17 +71,17 @@ fn main() {
     jupiter.details();
 
     // TODO
-    let saturn = Planet::Saturn(Rc::new(Sun {}));
+    let saturn = Planet::Saturn(Rc::clone(&sun));
     println!("reference count = {}", Rc::strong_count(&sun)); // 7 references
     saturn.details();
 
     // TODO
-    let uranus = Planet::Uranus(Rc::new(Sun {}));
+    let uranus = Planet::Uranus(Rc::clone(&sun));
     println!("reference count = {}", Rc::strong_count(&sun)); // 8 references
     uranus.details();
 
     // TODO
-    let neptune = Planet::Neptune(Rc::new(Sun {}));
+    let neptune = Planet::Neptune(Rc::clone(&sun));
     println!("reference count = {}", Rc::strong_count(&sun)); // 9 references
     neptune.details();
 
@@ -92,13 +102,13 @@ fn main() {
     drop(mars);
     println!("reference count = {}", Rc::strong_count(&sun)); // 4 references
 
-    // TODO
+    drop(earth);
     println!("reference count = {}", Rc::strong_count(&sun)); // 3 references
 
-    // TODO
+    drop(venus);
     println!("reference count = {}", Rc::strong_count(&sun)); // 2 references
 
-    // TODO
+    drop(mercury);
     println!("reference count = {}", Rc::strong_count(&sun)); // 1 reference
 
     assert_eq!(Rc::strong_count(&sun), 1);
